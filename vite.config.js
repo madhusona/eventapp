eeb7b12838a27005c02d96149f5cb078b491c3b5
madhusona <madhusona@gmail.com>
2021-10-12 11:13:07 +0530

    config

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+   
+    build: {     
+      outDir: 'docs',
+      rollupOptions: {
+        input:{
+          index:'index.html',
+          Gopi:'Gopinath/index.html',
+          indhu:'Indumathi/index.html',
+          kavi:'kaviyarasan/index.html'
+                         
+        }
+      }
+    }
+  }
