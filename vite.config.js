6afd8bef8f9461edc84667d803788d88ce5f5421
madhusona <madhusona@gmail.com>
2021-10-12 12:31:13 +0530

    github

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 module.exports = {
    
-    build: {     
-    
+    build: {  
+      
       rollupOptions: {
+        
         input:{
           index:'index.html',
           Gopi:'Gopinath/index.html',
           indhu:'Indhumathi/index.html',
-          kavi:'kaviyarasan/index.html'
-                         
+          kavi:'kaviyarasan/index.html'                         
+        },
+        external:{
+            css:'css/tailwind.css'
         }
       }
     }
